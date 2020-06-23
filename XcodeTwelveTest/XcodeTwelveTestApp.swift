e2d0aa7a2a338527d44e8e5e11376bc64a25e5cf
Mees Carels <mees.carels@hotmail.com>
2020-06-23 13:58:26 +0200

    Initial Commit

---
+++ XcodeTwelveTest/XcodeTwelveTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTwelveTestApp.swift
+//  XcodeTwelveTest
+//
+//  Created by Mees Carels on 23/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTwelveTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
